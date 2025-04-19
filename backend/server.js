efa64db9118d171217c5e79789dd0567f6edbe8a
sdSAHABUDDIN <syedsahabuddin182@gmail.com>
2025-04-19 17:20:52 +0530

    initial project setup:Created frontend and backend folders, install dependencies

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! This is the backend server.');
+});
+
+app.listen(PORT, () => {  
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+);
